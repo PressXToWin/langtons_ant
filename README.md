7f71ea376e197a56c5efaef5b01b19a02e5457ec
PressXToWin <PressXToWin@users.noreply.github.com>
2023-11-21 19:05:22 +0700

    removed unnecessary comments, add filename as parameter

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # langtons_ant
 
+Python 3.11
+
 Количество черных клеток: 3683
 
 ![](solution.png?raw=true)
--- main.py
+++ main.py
@@ -6,29 +6,29 @@ FIELD_WIDTH = 1024
 START_POSITION = (512, 512)
 
 
-def main(length, width, start):
-    ant_position = np.array(start)  # задали стартовую позицию
+def main(length, width, start, filename):
+    ant_position = np.array(start)
     current_direction = 0  # 0: вверх, 1: направо, 2: вниз, 4: налево
     directions = np.array([[-1, 0], [0, 1], [1, 0], [0, -1]])  # шаг в определённом направлении
-    field = np.ones((length, width), dtype=bool)  # создали поле
-    black_count = 0  # счётчик чёрных клеток
+    field = np.ones((length, width), dtype=bool)
+    black_count = 0
 
     while 0 <= ant_position[0] < length and 0 <= ant_position[1] < width:
-        is_white_cell = field[tuple(ant_position)]  # определили цвет клетки
+        is_white_cell = field[tuple(ant_position)]
         if is_white_cell:
-            current_direction = (current_direction + 1) % 4  # повернули по часовой стрелке
-            field[tuple(ant_position)] = False  # инвертировали цвет
-            black_count += 1  # посчитали клетку
+            current_direction = (current_direction + 1) % 4
+            field[tuple(ant_position)] = False
+            black_count += 1
         else:
-            current_direction = (current_direction - 1) % 4  # повернули против часовой стрелки
-            field[tuple(ant_position)] = True  # инвертировали цвет
-            black_count -= 1  # посчитали клетку
-        ant_position = (ant_position + directions[current_direction])  # сделали шаг
+            current_direction = (current_direction - 1) % 4
+            field[tuple(ant_position)] = True
+            black_count -= 1
+        ant_position = (ant_position + directions[current_direction])
 
-    img = Image.fromarray(field)  # преобразовали в изображение
-    img.save('solution.png')  # сохранили изображение
+    img = Image.fromarray(field)
+    img.save(filename)
     print(f'Количество черных клеток: {black_count}')
 
 
 if __name__ == '__main__':
-    main(FIELD_LENGTH, FIELD_WIDTH, START_POSITION)
+    main(FIELD_LENGTH, FIELD_WIDTH, START_POSITION, 'solution.png')
